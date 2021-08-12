7696b2523e27c96ba4b7be9293d1ccb896fc8e04
Christos Alexiou <pcnoic@gmail.com>
2021-08-13 00:19:47 +0300

    root commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,64 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <div class="q-pa-md">
+      <q-toolbar class="bg-purple text-white shadow-2 rounded-borders">
+        <q-btn
+          to="/"
+          flat
+          style="font-family: 'Pacifico', cursive"
+          label="🏡 Philoxenia"
+        />
+        <q-space />
+
+        <!--
+        notice shrink property since we are placing it
+        as child of QToolbar
+      -->
+        <q-tabs v-model="tab" shrink>
+          <q-route-tab
+            to="/offer"
+            style="font-family: 'Fredoka One', cursive"
+            name="offer"
+            label="Offer space"
+          />
+          <q-route-tab
+            to="/request"
+            style="font-family: 'Fredoka One', cursive"
+            name="tab2"
+            label="Request space"
+          />
+          <q-route-tab
+            to="/about"
+            style="font-family: 'Fredoka One', cursive"
+            name="tab3"
+            label="About"
+          />
+        </q-tabs>
+      </q-toolbar>
+    </div>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+</template>
+
+<script lang="ts">
+import { ref } from 'vue';
+
+export default {
+  name: 'MainLayout',
+
+  setup() {
+    return {
+      tab: ref(''),
+    };
+  },
+};
+</script>
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Fredoka+One&family=Pacifico&display=swap');
+</style>
---
+++ src/router/routes.ts
@@ -0,0 +1,22 @@
+import { RouteRecordRaw } from 'vue-router';
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/Index.vue'), name: 'Home'},
+      { path: '/offer', component: () => import('pages/Offer.vue'), name: 'Offer'}, 
+      { path: '/request', component: () => import('pages/Request.vue'), name: 'Request' }
+    ],
+  },
+
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: '/:catchAll(.*)*',
+    component: () => import('pages/Error404.vue'),
+  },
+];
+
+export default routes;
