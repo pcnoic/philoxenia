90e0b066a333eef9961f34306a9042d265ef2d83
Christos Alexiou <pcnoic@gmail.com>
2021-08-13 00:56:48 +0300

    make table responsive

--- client/src/pages/Index.vue
+++ client/src/pages/Index.vue
@@ -11,190 +11,121 @@
       icon="lightbulb_outline"
       to="/about"
     />
-    <div class="q-pa-md">
-      <q-table
-        style="height: 400px"
-        title="🏠 Latest spaces added"
-        :rows="rows"
-        :columns="columns"
-        row-key="index"
-        virtual-scroll
-        v-model:pagination="pagination"
-        :rows-per-page-options="[0]"
-      />
-    </div>
+<div class="q-pa-md">
+    <q-table
+      :grid="$q.screen.xs"
+      title="Latest spaces added 🏠"
+      :rows="rows"
+      :columns="columns"
+      row-key="name"
+      :filter="filter"
+      hide-header
+    >
+      <template v-slot:top-right>
+        <q-input borderless dense debounce="300" v-model="filter" placeholder="Search">
+          <template v-slot:append>
+            <q-icon name="search" />
+          </template>
+        </q-input>
+      </template>
+    </q-table>
+  </div>
   </q-page>
 </template>
-
 <script>
-import { ref } from 'vue';
+import { ref } from 'vue'
 
 const columns = [
   {
-    name: 'index',
-    label: '#',
-    field: 'index',
-  },
-  {
-    name: 'name',
+    name: 'desc',
     required: true,
     label: 'Dessert (100g serving)',
     align: 'left',
-    field: (row) => row.name,
-    format: (val) => `${val}`,
-    sortable: true,
+    field: row => row.name,
+    format: val => `${val}`,
+    sortable: true
   },
-  {
-    name: 'calories',
-    align: 'center',
-    label: 'Calories',
-    field: 'calories',
-    sortable: true,
-  },
+  { name: 'calories', align: 'center', label: 'Calories', field: 'calories', sortable: true },
   { name: 'fat', label: 'Fat (g)', field: 'fat', sortable: true },
   { name: 'carbs', label: 'Carbs (g)', field: 'carbs' },
-  { name: 'protein', label: 'Protein (g)', field: 'protein' },
-  { name: 'sodium', label: 'Sodium (mg)', field: 'sodium' },
-  {
-    name: 'calcium',
-    label: 'Calcium (%)',
-    field: 'calcium',
-    sortable: true,
-    sort: (a, b) => parseInt(a, 10) - parseInt(b, 10),
-  },
-  {
-    name: 'iron',
-    label: 'Iron (%)',
-    field: 'iron',
-    sortable: true,
-    sort: (a, b) => parseInt(a, 10) - parseInt(b, 10),
-  },
-];
+  { name: 'carbs', label: 'Carbs (g)', field: 'carbs' },
+  { name: 'carbs', label: 'Carbs (g)', field: 'carbs' },
+  { name: 'carbs', label: 'Carbs (g)', field: 'carbs' },
+  { name: 'carbs', label: 'Carbs (g)', field: 'carbs' }
+]
 
-const seed = [
+const rows = [
   {
     name: 'Frozen Yogurt',
     calories: 159,
     fat: 6.0,
-    carbs: 24,
-    protein: 4.0,
-    sodium: 87,
-    calcium: '14%',
-    iron: '1%',
+    carbs: 24
   },
   {
     name: 'Ice cream sandwich',
     calories: 237,
     fat: 9.0,
-    carbs: 37,
-    protein: 4.3,
-    sodium: 129,
-    calcium: '8%',
-    iron: '1%',
+    carbs: 37
   },
   {
     name: 'Eclair',
     calories: 262,
     fat: 16.0,
-    carbs: 23,
-    protein: 6.0,
-    sodium: 337,
-    calcium: '6%',
-    iron: '7%',
+    carbs: 23
   },
   {
     name: 'Cupcake',
     calories: 305,
     fat: 3.7,
-    carbs: 67,
-    protein: 4.3,
-    sodium: 413,
-    calcium: '3%',
-    iron: '8%',
+    carbs: 67
   },
   {
     name: 'Gingerbread',
     calories: 356,
     fat: 16.0,
-    carbs: 49,
-    protein: 3.9,
-    sodium: 327,
-    calcium: '7%',
-    iron: '16%',
+    carbs: 49
   },
   {
     name: 'Jelly bean',
     calories: 375,
     fat: 0.0,
-    carbs: 94,
-    protein: 0.0,
-    sodium: 50,
-    calcium: '0%',
-    iron: '0%',
+    carbs: 94
   },
   {
     name: 'Lollipop',
     calories: 392,
     fat: 0.2,
-    carbs: 98,
-    protein: 0,
-    sodium: 38,
-    calcium: '0%',
-    iron: '2%',
+    carbs: 98
   },
   {
     name: 'Honeycomb',
     calories: 408,
     fat: 3.2,
-    carbs: 87,
-    protein: 6.5,
-    sodium: 562,
-    calcium: '0%',
-    iron: '45%',
+    carbs: 87
   },
   {
     name: 'Donut',
     calories: 452,
     fat: 25.0,
-    carbs: 51,
-    protein: 4.9,
-    sodium: 326,
-    calcium: '2%',
-    iron: '22%',
+    carbs: 51
   },
   {
     name: 'KitKat',
     calories: 518,
     fat: 26.0,
-    carbs: 65,
-    protein: 7,
-    sodium: 54,
-    calcium: '12%',
-    iron: '6%',
-  },
-];
-
-// we generate lots of rows here
-let rows = [];
-for (let i = 0; i < 1000; i++) {
-  rows = rows.concat(seed.slice(0).map((r) => ({ ...r })));
-}
-rows.forEach((row, index) => {
-  row.index = index;
-});
+    carbs: 65
+  }
+]
 
 export default {
-  setup() {
+  setup () {
     return {
+      filter: ref(''),
       columns,
-      rows,
-
-      pagination: ref({
-        rowsPerPage: 0,
-      }),
-    };
-  },
-};
+      rows
+    }
+  }
+}
 </script>
 
 <style scoped>
