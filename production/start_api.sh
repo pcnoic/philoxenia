c1bbbd44bc4bc0016409e368e11e86620bc3a2d0
Christos Alexiou <pcnoic@gmail.com>
2021-08-28 19:11:26 +0300

    bind gunicorn to unix socket instead of net address

--- production/start_api.sh
+++ production/start_api.sh
@@ -3,7 +3,7 @@
 API_LOCATION="/api"
 
 cd ${API_LOCATION}
-gunicorn -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000 --workers 1 --threads 8 --timeout 0 api:app &
+gunicorn -k uvicorn.workers.UvicornWorker --bind unix:/tmp/gunicorn.sock --workers 1 --threads 8 --timeout 0 api:app &
 echo "Philoxenia API has been started."
 
 nginx -g "daemon off;"
