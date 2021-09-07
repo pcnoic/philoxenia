bf5ca6e4c421fb2387e92d89663c4703784d8f96
Christos Alexiou <pcnoic@gmail.com>
2021-09-07 23:14:38 +0300

    rename script

---
+++ local_env.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Best chances are sourcing the script. The script is used for reference.
+
+## Required enviroment variables.
+# Database
+export MONGODB_HOST="mongodb://localhost:27017"
+export MONGODB_NAME="foteini"
+
+# Application
+
+# Misc
